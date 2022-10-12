555e7f2249f28a0efbe2dfc5ac33ab606223104f
DevHobbyist <l3alazskiss15@gmail.com>
2022-10-12 11:29:11 -0300

    -

--- Assets/Scripts/PhysicsPickup.cs
+++ Assets/Scripts/PhysicsPickup.cs
@@ -9,7 +9,7 @@ public class PhysicsPickup : MonoBehaviour
     [SerializeField] private Transform PickupTarget;
     [Space]
     [SerializeField] private float PickupRange;
-    private Rigidbody CuurentObject;
+    private Rigidbody CurrentObject;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,31 @@ public class PhysicsPickup : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.E))
         {
+
+            if(CurrentObject)
+            {
+                CurrentObject.useGravity = true;
+                CurrentObject = null;
+                return;
+            }
+
             Ray CameraRay = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if(Physics.Raycast(CameraRay, out RaycastHit HitInfo, PickupRange, PickupMask))
+            {
+                CurrentObject = HitInfo.rigidbody;
+                CurrentObject.useGravity = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (CurrentObject)
+        {
+            Vector3 DirectionToPoint = PickupTarget.position - CurrentObject.position;
+            float DistanceToPoint = DirectionToPoint.magnitude;
+
+            CurrentObject.velocity = DirectionToPoint * 12f * DistanceToPoint;
         }
     }
 }
